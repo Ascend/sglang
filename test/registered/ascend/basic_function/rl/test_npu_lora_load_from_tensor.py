cb9ceb51a6daf30f8d1b931dffd1163def4412c1
YZY00Raiser <2452658175@qq.com>
2026-05-28 13:11:15 +0800

    Fix: Remove unused imports

--- test/registered/ascend/basic_function/rl/test_npu_lora_load_from_tensor.py
+++ test/registered/ascend/basic_function/rl/test_npu_lora_load_from_tensor.py
@@ -5,18 +5,12 @@ import unittest
 from safetensors.torch import load_file
 
 import sglang as sgl
-from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import (
     LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH,
     LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
 )
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
-    CustomTestCase,
-    popen_launch_server,
-)
+from sglang.test.test_utils import CustomTestCase
 
 register_npu_ci(est_time=150, suite="nightly-2-npu-a3", nightly=True)
 
