440cbf4ee9fb2ec7bfe48b94ca68219baf2d268c
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-06-15 14:27:42 +0800

    lint checks

--- test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms_aime26.py
+++ test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms_aime26.py
@@ -17,11 +17,11 @@ register_npu_ci(
     nightly=True,
     disabled="performance testcase",
 )
-
+QWEN3_6_35B_A3B_MODEL_PATH="/home/weights/Qwen3.6-35B-A3B"
 QWEN3_6_35B_A3B_64K_PREFIX_ENVS = {
-    # "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
     "STREAMS_PER_DEVICE": "32",
-    "HCCL_BUFFSIZE": "1",
+    "HCCL_BUFFSIZE": "300",
     "HCCL_SOCKET_IFNAME": "lo",
     "GLOO_SOCKET_IFNAME": "lo",
     "HCCL_OP_EXPANSION_MODE": "AIV",
@@ -32,7 +32,6 @@ QWEN3_6_35B_A3B_64K_PREFIX_ENVS = {
     "GDN_ATTN_BACKEND_TRITON": "1",
 }
 
-
 QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     "--tp-size",
     2,
@@ -42,6 +41,8 @@ QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     "ascend",
     "--device",
     "npu",
+    "--chunked-prefill-size",
+    -1,
     "--max-prefill-tokens",
     65536,
     "--trust-remote-code",
@@ -50,18 +51,16 @@ QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     "--max-running-requests",
     40,
     "--max-mamba-cache-size",
-    160,
+    210,
     "--mem-fraction-static",
-    0.75,
+    0.71,
     "--cuda-graph-bs",
-    1,
     2,
-    3,
-    4,
-    6,
-    12,
-    28,
-    35,
+    8,
+    16,
+    24,
+    32,
+    36,
     40,
     "--enable-multimodal",
     "--mm-attention-backend",
@@ -78,36 +77,11 @@ QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     1,
     "--speculative-num-draft-tokens",
     4,
+    "--base-gpu-id",
+    "4",
+    "--port",
+    "0809",
 ]
-
-class TestNPUQwen3_6_35BA3B_1P_In64k_Out1k_Prefix90_50ms(
-    TestAscendPerformanceTestCaseBase
-):
-    """Test NPU performance for Qwen3.6-35B-A3B 1p in64k out1k prefix90 50ms"""
-
-    base_url = DEFAULT_URL_FOR_TEST
-    benchmark_tool = BENCHMARK_TOOL_DEFAULT
-    model = QWEN3_6_35B_A3B_MODEL_PATH
-    other_args = QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS
-    envs = QWEN3_6_35B_A3B_64K_PREFIX_ENVS
-    dataset_name = "generated-shared-prefix"
-    max_concurrency = 40
-    num_prompts = 40
-    input_len = 65536
-    output_len = 1024
-    random_range_ratio = 1
-    repeat_rate = 0.9
-    tpot = 50
-    request_rate = float("inf")
-    output_token_throughput = 660
-
-    # @classmethod
-    # def setUpClass(cls):
-    #     pass
-
-    def test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms(self):
-        """Run NPU performance test for Qwen3.6-35B-A3B in64k out1k prefix90 50ms"""
-        self.run_throughput()
 
 
 # class TestNPUQwen3_6_35BA3B_1P_AIME2026(TestAscendAccuracyTestCaseBase):
@@ -126,13 +100,44 @@ class TestNPUQwen3_6_35BA3B_1P_In64k_Out1k_Prefix90_50ms(
 #         "repetition_penalty": 1.08,
 #     }
 
-#     # @classmethod
-#     # def tearDownClass(cls):
-#     #     pass
+#     @classmethod
+#     def tearDownClass(cls):
+#         pass
 
 #     def test_npu_qwen3_6_35b_a3b_1p_aime2026(self):
 #         """Run NPU accuracy test for Qwen3.6-35B-A3B on AIME2026"""
-#         # self.run_accuracy()
+#         self.run_accuracy()
+
+
+class TestNPUQwen3_6_35BA3B_1P_In64k_Out1k_Prefix90_50ms(
+    TestAscendPerformanceTestCaseBase
+):
+    """Test NPU performance for Qwen3.6-35B-A3B 1p in64k out1k prefix90 50ms"""
+
+    base_url = DEFAULT_URL_FOR_TEST
+    benchmark_tool = BENCHMARK_TOOL_DEFAULT
+    model = QWEN3_6_35B_A3B_MODEL_PATH
+    other_args = QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS
+    envs = QWEN3_6_35B_A3B_64K_PREFIX_ENVS
+    dataset_name = "generated-shared-prefix"
+    dataset_path = "/home/hexq/ShareGPT_V3_unfiltered_cleaned_split.json"
+    max_concurrency = 40
+    num_prompts = 40
+    input_len = 65536
+    output_len = 1024
+    random_range_ratio = 1
+    repeat_rate = 0.9
+    tpot = 50
+    request_rate = float("inf")
+    output_token_throughput = 660
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms(self):
+        """Run NPU performance test for Qwen3.6-35B-A3B in64k out1k prefix90 50ms"""
+        self.run_throughput()
 
 
 if __name__ == "__main__":
