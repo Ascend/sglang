3ae6e9418962e8fbfa956ca2883450c0620119fe
YZY00Raiser <2452658175@qq.com>
2026-06-17 17:17:20 +0800

    Restore GSM8KMixin with lower threshold 0.35

--- test/registered/ascend/basic_function/dp_engine/test_npu_data_parallelism.py
+++ test/registered/ascend/basic_function/dp_engine/test_npu_data_parallelism.py
@@ -6,6 +6,7 @@ import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import QWEN3_0_6B_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -16,7 +17,9 @@ from sglang.test.test_utils import (
 register_npu_ci(est_time=100, suite="full-2-npu-a3", nightly=True)
 
 
-class TestDataParallelism(CustomTestCase):
+class TestDataParallelism(CustomTestCase, GSM8KMixin):
+    gsm8k_accuracy_thres = 0.35
+
     @classmethod
     def setUpClass(cls):
         cls.model = QWEN3_0_6B_WEIGHTS_PATH
