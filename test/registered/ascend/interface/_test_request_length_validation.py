c7b902ab0d8ae59b1879bc268db1b99729fc7bfd
YZY00Raiser <2452658175@qq.com>
2026-06-25 18:25:49 +0800

    style: apply black formatting to test_request_length_validation

--- test/registered/ascend/interface/_test_request_length_validation.py
+++ test/registered/ascend/interface/_test_request_length_validation.py
@@ -16,7 +16,6 @@ from sglang.test.test_utils import (
 register_npu_ci(est_time=400, suite="full-1-npu-a3", nightly=True)
 
 
-
 class TestRequestLengthValidation(CustomTestCase):
     @classmethod
     def setUpClass(cls):
