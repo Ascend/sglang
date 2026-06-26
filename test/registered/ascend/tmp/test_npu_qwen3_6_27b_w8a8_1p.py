ec41ba91c83f18919efec57253a2d1d747c7ee97
cy <chenyang08056032@163.com>
2026-06-26 16:56:39 +0800

    adapt

---
+++ test/registered/ascend/tmp/test_npu_qwen3_6_27b_w8a8_1p.py
@@ -0,0 +1,185 @@
+import os
+import unittest
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.e2e.test_npu_performance_utils import (
+    QWEN3_6_27B_W8A8_MODEL_PATH,
+)
+from sglang.test.ascend.test_ascend_utils import (
+    logger,
+    run_command,
+)
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(
+    est_time=3600,
+    suite="",
+    nightly=True,
+    disabled="performance testcase",
+)
+
+QWEN3_6_27B_3K5_1K5_ENVS = {
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "STREAMS_PER_DEVICE": "32",
+    "HCCL_SOCKET_IFNAME": "lo",
+    "GLOO_SOCKET_IFNAME": "lo",
+    "HCCL_OP_EXPANSION_MODE": "AIV",
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "SGLANG_ENABLE_SPEC_V2": "1",
+    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "0",
+    "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
+    "SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES": "130",
+    "ASCEND_USE_FIA": "1",
+}
+
+QWEN3_6_27B_3K5_1K5_OTHER_ARGS = [
+    "--tp-size",
+    2,
+    "--nnodes",
+    1,
+    "--attention-backend",
+    "ascend",
+    "--device",
+    "npu",
+    "--chunked-prefill-size",
+    -1,
+    "--max-prefill-tokens",
+    60000,
+    "--disable-radix-cache",
+    "--trust-remote-code",
+    "--max-running-requests",
+    64,
+    "--max-mamba-cache-size",
+    74,
+    "--mem-fraction-static",
+    0.7,
+    "--cuda-graph-bs",
+    2,
+    8,
+    16,
+    32,
+    40,
+    45,
+    50,
+    54,
+    "--enable-multimodal",
+    "--quantization",
+    "modelslim",
+    "--mm-attention-backend",
+    "ascend_attn",
+    "--dtype",
+    "bfloat16",
+    "--mamba-ssm-dtype",
+    "bfloat16",
+    "--speculative-algorithm",
+    "NEXTN",
+    "--speculative-num-steps",
+    3,
+    "--speculative-eagle-topk",
+    1,
+    "--speculative-num-draft-tokens",
+    4,
+    "--reasoning-parser",
+    "qwen3",
+    "--tool-call-parser",
+    "qwen3_coder",
+]
+
+cmd = "npu-smi info"
+
+
+class TestNPUQwen3_6_27B_1P_In3k5_Out1k5_50ms(CustomTestCase):
+    model = QWEN3_6_27B_W8A8_MODEL_PATH
+    other_args = QWEN3_6_27B_3K5_1K5_OTHER_ARGS
+    envs = QWEN3_6_27B_3K5_1K5_ENVS
+    out = open(f"./out_log.txt", "w+", encoding="utf-8")
+    err = open(f"./err_log.txt", "w+", encoding="utf-8")
+
+    @classmethod
+    def setUpClass(cls):
+        raw_result = run_command(cmd)
+        logger.info("S1、服务启动前执行npu-smi info")
+        logger.info(raw_result)
+
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        env = os.environ.copy()
+        for key, value in env.items():
+            logger.info(f"ENV_VAR_SYS {key}:{value}")
+        if cls.envs:
+            for key, value in cls.envs.items():
+                logger.info(f"ENV_VAR_CASE {key}:{value}")
+                env[key] = value
+
+        other_args = list(cls.other_args)
+
+        logger.info("S2、启动服务")
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+            env=env,
+            return_stdout_stderr=(cls.out, cls.err),
+        )
+
+    def test_1(self):
+        logger.info("S3、记录HCCL初始化完成后、模型加载前，通信域内存占用")
+        res1 = run_command("cat ./out_log.txt | grep 'Init torch distributed ends'")
+        logger.info(res1)
+        logger.info("S4、记录模型加载后，模型权重内存占用")
+        res2 = run_command("cat ./out_log.txt | grep 'Load weight end'")
+        logger.info(res2)
+        logger.info("S5、记录KV cache分配后，KV cache内存占用")
+        res3 = run_command("cat ./out_log.txt | grep 'KV Cache is allocated'")
+        logger.info(res3)
+        logger.info("S6、记录NPU graph buffer分配后，NPU graph buffer内存占用")
+        res4 = run_command("cat ./out_log.txt | grep 'Capture npu graph end'")
+        logger.info(res4)
+        logger.info("S7、服务启动成功后执行npu-smi info")
+        raw_result = run_command(cmd)
+        logger.info(raw_result)
+
+    def test_2(self):
+        logger.info("S9、curl一条请求，完成后记录每张卡的HBM内存占用和总内存")
+        requests.post(
+            f"{self.base_url}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 100,
+                },
+            },
+        )
+        raw_result = run_command(cmd)
+        logger.info(raw_result)
+
+    def test_3(self):
+        if hasattr(self, "process") and self.process:
+            try:
+                kill_process_tree(self.process.pid)
+            except Exception as e:
+                logger.error(f"Error during tearDown: {e}")
+        logger.info("S9、停止服务，等待服务完全停止后，记录每张卡的HBM内存占用和总内")
+        raw_result = run_command(cmd)
+        logger.info(raw_result)
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, "process") and cls.process:
+            try:
+                kill_process_tree(cls.process.pid)
+            except Exception as e:
+                logger.error(f"Error during tearDown: {e}")
+
+
+if __name__ == "__main__":
+    unittest.main()
