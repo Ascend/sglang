85e3e62c45463382e5e0fc661ae377420844318d
Kurkur <mccllm@qq.com>
2026-07-07 18:54:09 +0800

    ci test

--- test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_2p_in64k_out1k_prefix90_50ms.py
+++ test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_2p_in64k_out1k_prefix90_50ms.py
@@ -94,7 +94,7 @@ class TestNPUQwen3_6_27B_1P_In64k_Out1k_Prefix90_50ms(
     envs = QWEN3_6_27B_64K_PREFIX_ENVS
     dataset_name = "generated-shared-prefix"
     max_concurrency = 20
-    num_prompts = 20
+    num_prompts = 40
     input_len = 64000
     output_len = 1000
     random_range_ratio = 1
