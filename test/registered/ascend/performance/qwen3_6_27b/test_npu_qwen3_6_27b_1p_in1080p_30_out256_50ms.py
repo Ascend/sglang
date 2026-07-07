303705dc7361bb6c04de28a41f331753d95b279f
Hexq0210 <893781835@qq.com>
2026-07-07 23:18:45 +0800

    Update test_npu_qwen3_6_27b_1p_in1080p_30_out256_50ms.py

--- test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_1p_in1080p_30_out256_50ms.py
+++ test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_1p_in1080p_30_out256_50ms.py
@@ -89,7 +89,7 @@ QWEN3_6_27B_1080P_OTHER_ARGS = [
     1,
     "--speculative-num-draft-tokens",
     4,
-    "--mm-enable-dp-encoder",
+    # "--mm-enable-dp-encoder",
     "--reasoning-parser",
     "qwen3",
     "--tool-call-parser",
@@ -114,9 +114,10 @@ class TestNPUQwen3_6_27B_1P_In1080p_30_Out256_50ms(TestAscendPerformanceTestCase
     random_range_ratio = 1
     image_resolution = "1920x1080"
     image_count = 1
-    # seed = 1
+    seed = 1
     tpot = 50
     output_token_throughput = 226
+    skip_warmup=True
 
     def test_npu_qwen3_6_27b_1p_in1080p_30_out256_50ms(self):
         """Run NPU performance test for Qwen3.6-27B in1080p 30 out256 50ms"""
