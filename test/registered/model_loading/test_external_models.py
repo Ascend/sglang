92d9410794f219c4679df357363e994fe67fe993
l30056750 <1964551841@qq.com>
2026-07-08 16:43:32 +0800

    GPU case transplantation

--- test/registered/model_loading/test_external_models.py
+++ test/registered/model_loading/test_external_models.py
@@ -18,16 +18,13 @@ class TestExternalModelsNPU(CustomTestCase):
         envs.SGLANG_EXTERNAL_MODEL_PACKAGE.set("sglang.test.external_models")
         envs.SGLANG_EXTERNAL_MM_PROCESSOR_PACKAGE.set("sglang.test.external_models")
         prompt = "Today is a sunny day and I like"
-        model_path = "Qwen/Qwen2-VL-2B-Instruct"
+        model_path = "/home/weights/Qwen/Qwen2-VL-2B-Instruct"
 
         engine = sgl.Engine(
             model_path=model_path,
-            cuda_graph_max_bs=1,
             max_total_tokens=64,
             enable_multimodal=True,
             attention_backend="torch_native",
-            disable_cuda_graph=True,
-            disable_radix_cache=True,
         )
         out = engine.generate(prompt)["text"]
         engine.shutdown()
