0c8d053d7a671fdd037ded873a6e57cb3eedb88f
chx96642264 <chx96642264@163.com>
2026-07-08 20:04:40 +0800

    ds-r1, 1p1d, 7

--- test/registered/ascend/performance/deepseek_r1/test_npu_deepseek_r1_w4a8_1p1d_16p_in3k5_out1k5_50ms.py
+++ test/registered/ascend/performance/deepseek_r1/test_npu_deepseek_r1_w4a8_1p1d_16p_in3k5_out1k5_50ms.py
@@ -171,8 +171,8 @@ class TestDeepSeekR1W4A8(TestAscendPerfMultiNodePdSepTestCaseBase):
     input_len = 3584
     output_len = 1536
     random_range_ratio = 1
-    seed = 1
     tpot = 50
+    seed = 1
     output_token_throughput = 7947.95
 
     def test_throughput(self):
