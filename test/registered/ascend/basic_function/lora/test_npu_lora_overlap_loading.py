080e90eaa94818b4c5748409c71409d19f2c08a7
YZY00Raiser <2452658175@qq.com>
2026-07-15 20:21:24 +0800

    update

--- test/registered/ascend/basic_function/lora/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/lora/test_npu_lora_overlap_loading.py
@@ -7,7 +7,7 @@ from sglang.test.ascend.lora_utils import (
 )
 from sglang.test.test_utils import CustomTestCase
 
-register_npu_ci(est_time=600, suite="full-1-npu-a3", nightly=True)
+register_npu_ci(est_time=300, suite="full-1-npu-a3", nightly=True)
 
 
 class TestLoRAOverlapLoading(CustomTestCase):
