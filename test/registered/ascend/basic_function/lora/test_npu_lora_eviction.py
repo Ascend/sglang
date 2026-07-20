6aadd76ce6c5b3e97180f583ae7458a670fef325
YZY00Raiser <2452658175@qq.com>
2026-07-20 10:59:37 +0800

    update

--- test/registered/ascend/basic_function/lora/test_npu_lora_eviction.py
+++ test/registered/ascend/basic_function/lora/test_npu_lora_eviction.py
@@ -75,7 +75,7 @@ class TestLoRAEviction(CustomTestCase):
     ):
         REUSED_LORA_NAME = "lora"
         max_new_tokens = 256
-        torch_dtype = torch.float16
+        torch_dtype = torch.bfloat16
         base_path = LLAMA_3_1_8B_INSTRUCT_WEIGHTS_PATH
         assert len(lora_paths) >= 2
 
