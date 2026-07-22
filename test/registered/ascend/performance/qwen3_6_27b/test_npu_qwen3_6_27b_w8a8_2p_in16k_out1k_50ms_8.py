449aea6a4449e5adb5bf64b69f7150e58a74aff7
Kurkur <mccllm@qq.com>
2026-07-22 09:26:09 +0800

    qwen3.6

--- test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_w8a8_2p_in16k_out1k_50ms_8.py
+++ test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_w8a8_2p_in16k_out1k_50ms_8.py
@@ -46,9 +46,9 @@ QWEN3_6_27B_16K_1k_OTHER_ARGS = [
     "--disable-radix-cache",
     "--trust-remote-code",
     "--max-running-requests",
-    37,
+    25,
     "--max-mamba-cache-size",
-    74,
+    50,
     "--mem-fraction-static",
     0.70,
     "--cuda-graph-bs",
@@ -69,18 +69,18 @@ QWEN3_6_27B_16K_1k_OTHER_ARGS = [
     23,
     24,
     25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31,
-    32,
-    33,
-    34,
-    35,
-    36,
-    37,
+    # 26,
+    # 27,
+    # 28,
+    # 29,
+    # 30,
+    # 31,
+    # 32,
+    # 33,
+    # 34,
+    # 35,
+    # 36,
+    # 37,
     # 36,
     # 38,
     "--quantization",
@@ -113,9 +113,9 @@ class TestNPUQwen3_6_27B_2P_In16k_Out1k_50ms(TestAscendPerformanceTestCaseBase):
     other_args = QWEN3_6_27B_16K_1k_OTHER_ARGS
     envs = QWEN3_6_27B_16K_1k_ENVS
     dataset_name = "random"
-    max_concurrency = 33
+    max_concurrency = 25
     warmup_requests = 4
-    num_prompts = 66
+    num_prompts = 100
     input_len = 16000
     output_len = 1000
     random_range_ratio = 1
