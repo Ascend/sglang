862fc69047cdc61db70b5213e20fe782ba7132da
loading66 <448274043@qq.com>
2026-07-29 11:35:38 +0800

    fix bug

--- python/sglang/test/ascend/e2e/run_npu_testcase.sh
+++ python/sglang/test/ascend/e2e/run_npu_testcase.sh
@@ -113,8 +113,8 @@ if [ -n "${COVERAGE_FILE}" ]; then
     python -c "import coverage" 2>/dev/null || pip install coverage --retries 3 || pip install coverage -i https://pypi.tuna.tsinghua.edu.cn/simple/
     # Enable subprocess coverage tracking via .pth file and COVERAGE_PROCESS_START
     SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
-    echo "import numpy; import torch; import torchvision" > "${SITE_PACKAGES}/a1_coverage.pth"
-    echo "import coverage; coverage.process_startup()" > "${SITE_PACKAGES}/coverage.pth"
+    echo "import numpy; import torch; import torchvision; import triton; import requests; import coverage; coverage.process_startup()" > "${SITE_PACKAGES}/coverage.pth"
+    rm -f $(python -c "import site; print(site.getsitepackages()[0])")/a1_coverage.pth
     export COVERAGE_RCFILE="/data/ascend-ci-share-pkking-sglang/coverage_data/.coveragerc"
     export COVERAGE_PROCESS_START="/data/ascend-ci-share-pkking-sglang/coverage_data/.coveragerc"
     mkdir -p /data/ascend-ci-share-pkking-sglang/coverage_data
