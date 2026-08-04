cd72b1bb2f5ef2c6a30f59646ced8325d62b1697
YZY00Raiser <2452658175@qq.com>
2026-08-04 14:11:34 +0800

    update

--- test/registered/ascend/basic_function/lora/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/lora/test_npu_lora_overlap_loading.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 import unittest
 
 from sglang.test.ascend.lora_utils import (
-    LORA_MODELS_QWEN3,
+    CI_MULTI_LORA_MODELS,
     run_lora_batch_splitting_equivalence_test,
 )
 from sglang.test.ci.ci_register import register_npu_ci
@@ -15,7 +15,7 @@ class TestLoRAOverlapLoading(CustomTestCase):
 
     def test_ci_lora_models_batch_splitting(self):
         run_lora_batch_splitting_equivalence_test(
-            LORA_MODELS_QWEN3,
+            CI_MULTI_LORA_MODELS,
             enable_lora_overlap_loading=True,
         )
 
