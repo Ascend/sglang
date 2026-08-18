aa238668192ea7de948afd97741531a941257b75
yangfen0903 <2253770994@qq.com>
2026-08-18 18:04:27 +0800

    testcases

--- test/registered/npu/performance/glm5_1/test_npu_glm5_1_w4a8_1p1d_32p_in128k_out1k_20ms.py
+++ test/registered/npu/performance/glm5_1/test_npu_glm5_1_w4a8_1p1d_32p_in128k_out1k_20ms.py
@@ -156,8 +156,6 @@ GLM_5_1_PD_SEP_DECODE_ARGS = [
     180000,
     "--tokenizer-worker-num",
     16,
-    "--tokenizer-worker-num",
-    16,
     "--prefill-round-robin-balance",
     "--disable-shared-experts-fusion",
     "--dtype",
