2dd9d9df122d894a53969834f688629df4ff2d6f
cy <chenyang08056032@163.com>
2026-08-21 16:51:11 +0800

    Based on the GSM8K accuracy of the official LLM-Research/Llama-2-7B model, modify the baseline.

--- test/registered/npu/llm_models/test_npu_llama_2_7b_communications_compression.py
+++ test/registered/npu/llm_models/test_npu_llama_2_7b_communications_compression.py
@@ -9,14 +9,14 @@ register_npu_ci(est_time=400, suite="full-2-npu-a3", nightly=True)
 
 
 class TestLlama(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the LLM-Research/Llama-2-7B model on the GSM8K dataset with tp communications quantization is no less than 0.18.
+    """Testcase: Verify that the inference accuracy of the LLM-Research/Llama-2-7B model on the GSM8K dataset with tp communications quantization is no less than 0.15.
 
     [Test Category] Model
     [Test Target] LLM-Research/Llama-2-7B
     """
 
     model = LLAMA_2_7B_WEIGHTS_PATH
-    accuracy = 0.18
+    accuracy = 0.15
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
