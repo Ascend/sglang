fd4350a2d5fd97f5a3ecc866f45b41b2caac639c
g30001723 <guoxin55@h-partners.com>
2026-08-25 09:59:07 +0800

    test

--- test/registered/npu/vlm_models/test_npu_vision_openai_server_a.py
+++ test/registered/npu/vlm_models/test_npu_vision_openai_server_a.py
@@ -3,19 +3,12 @@ Usage:
 python3 -m unittest test_vision_openai_server.TestOpenAIVisionServer.test_mixed_batch
 python3 -m unittest test_vision_openai_server.TestOpenAIVisionServer.test_multi_images_chat_completion
 """
+
 import os
 import unittest
 
-import openai
-
 from sglang.test.ascend.test_ascend_utils import (
-    KIMI_VL_A3B_INSTRUCT_WEIGHTS_PATH,
-    LLAVA_ONEVISION_QWEN2_7B_OV_WEIGHTS_PATH,
-    MINICPM_O_2_6_WEIGHTS_PATH,
-    MINICPM_V_2_6_WEIGHTS_PATH,
-    QWEN2_VL_2B_INSTRUCT_WEIGHTS_PATH,
-    QWEN3_VL_8B_INSTRUCT_WEIGHTS_PATH,
-    QWEN3_VL_30B_A3B_INSTRUCT_WEIGHTS_PATH, GEMMA_4_31B_WEIGHTS_PATH,
+    GEMMA_4_31B_WEIGHTS_PATH,
 )
 from sglang.test.ascend.vlm_utils import *
 from sglang.test.ascend.vlm_utils import (
@@ -156,6 +149,7 @@ class TestGemma4itServer(ImageOpenAITestMixin):
         "--tp-size",
         "4",
     ]
+
 
 #
 # class TestKimiVLServer(ImageOpenAITestMixin):
