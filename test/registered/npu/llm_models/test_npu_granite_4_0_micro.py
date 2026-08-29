77760751758efe672cbdab3f2f8a671ed017e76d
cy <chenyang08056032@163.com>
2026-08-29 10:41:39 +0800

    test

--- test/registered/npu/llm_models/test_npu_granite_4_0_micro.py
+++ test/registered/npu/llm_models/test_npu_granite_4_0_micro.py
@@ -9,30 +9,15 @@ register_npu_ci(est_time=400, suite="full-1-npu-a3", nightly=True)
 
 
 class TestGranite40Micro(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the ibm-granite/granite-4.0-micro model on the GSM8K dataset is no less than 0.8545.
+    """Testcase: Verify that the inference accuracy of the ibm-granite/granite-4.0-micro model on the GSM8K dataset is no less than 0.78.
 
     [Test Category] Model
     [Test Target] ibm-granite/granite-4.0-micro
     """
 
     model = GRANITE_4_0_MICRO_WEIGHTS_PATH
-    accuracy = 0
+    accuracy = 0.78
     gsm8k_num_shots = 8
-    num_questions = 1319
-    max_tokens = 1024
-
-
-class TestGranite40Micro_200(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the ibm-granite/granite-4.0-micro model on the GSM8K dataset is no less than 0.8545.
-
-    [Test Category] Model
-    [Test Target] ibm-granite/granite-4.0-micro
-    """
-
-    model = GRANITE_4_0_MICRO_WEIGHTS_PATH
-    accuracy = 0
-    gsm8k_num_shots = 8
-    max_tokens = 1024
 
 
 if __name__ == "__main__":
