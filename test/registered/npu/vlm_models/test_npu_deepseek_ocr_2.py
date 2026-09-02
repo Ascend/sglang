1c14c987107b2c8d46caa97f88345697985653d7
cy <chenyang08056032@163.com>
2026-09-02 09:13:29 +0800

    Return DTS

--- test/registered/npu/vlm_models/test_npu_deepseek_ocr_2.py
+++ test/registered/npu/vlm_models/test_npu_deepseek_ocr_2.py
@@ -13,7 +13,7 @@ from sglang.test.ci.ci_register import register_npu_ci
 # )
 
 register_npu_ci(
-    est_time=400,
+    est_time=3600,
     suite="nightly-acc-debug-2-npu-a3",
     nightly=True,
 )
