94ccd4f954be8bda617b5891cc888fb406230c2e
YZY00Raiser <2452658175@qq.com>
2026-09-03 11:50:20 +0800

    update

--- test/registered/npu/llm_models/test_npu_qwen3_30b_fuseep.py
+++ test/registered/npu/llm_models/test_npu_qwen3_30b_fuseep.py
@@ -42,8 +42,10 @@ class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
         "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "188416",
     }
 
+
 class TestQwen330Bw8a8FuseModeWithOne(TestQwen330Bw8a8FuseModeWithTwo):
     fuseep_mode = 1
+
 
 if __name__ == "__main__":
     unittest.main()
