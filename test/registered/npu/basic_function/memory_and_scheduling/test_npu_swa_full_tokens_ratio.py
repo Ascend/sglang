8a1112c2a66893fe81d8812da1db46aba3a25429
HeYao <heyao@example.com>
2026-09-07 14:16:52 +0800

    Test testcases

--- test/registered/npu/basic_function/memory_and_scheduling/test_npu_swa_full_tokens_ratio.py
+++ test/registered/npu/basic_function/memory_and_scheduling/test_npu_swa_full_tokens_ratio.py
@@ -12,6 +12,7 @@ import os
 import re
 import tempfile
 import unittest
+
 from sglang.test.ascend.e2e.test_npu_accuracy_utils import (
     BENCHMARK_TOOL_DEFAULT,
     TestNpuAccuracyTestCaseBase,
