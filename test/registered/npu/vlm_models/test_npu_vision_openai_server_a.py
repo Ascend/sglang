9435153d3fd55ca99094d6578417f540b431d30a
HeYao <heyao@example.com>
2026-09-17 20:43:58 +0800

    update testcases

--- test/registered/npu/vlm_models/test_npu_vision_openai_server_a.py
+++ test/registered/npu/vlm_models/test_npu_vision_openai_server_a.py
@@ -33,6 +33,7 @@ from sglang.test.ci.ci_register import register_npu_ci
 register_npu_ci(est_time=300, suite="validate-cleanup-npu", nightly=True)
 
 class TestLlavaServer(ImageOpenAITestMixin):
+    os.environ.setdefault("REQUEST_TIMEOUT", "40")
     model = LLAVA_ONEVISION_QWEN2_7B_OV_WEIGHTS_PATH
     extra_args = [
         "--attention-backend",
